da1f15d51e37fae6fe53c7101269946e3f449d14
Tanushwi Singh <156989068+Tanushwi@users.noreply.github.com>
2024-10-25 20:57:47 +0530

    Add files via upload

---
+++ new and delete operator.cpp
@@ -0,0 +1,17 @@
+//Q. Sum of n array elements
+
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//    int n,sum=0;
+//    cin>>n;
+//    int *a=new int[n];
+//    for(int i=0;i<n;i++)
+//    {
+//        cin>>a[i];
+//        sum=sum+a[i];
+//    }
+//    cout<<"Sum of "<<n<<" array elements is:"<<sum;
+//    return 0;
+//}
