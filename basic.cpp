248b36cb100f4195e7b5716f25c2f93e080203e2
Tanushwi Singh <156989068+Tanushwi@users.noreply.github.com>
2024-10-25 20:56:06 +0530

    Add files via upload

---
+++ basic.cpp
@@ -0,0 +1,66 @@
+//Q1. WAP to add 2 numbers
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int a,b;
+//     cin>>a>>b;
+//     cout<<"The sum is:"<<a+b;
+//     return 0;
+// }
+
+//Q2. Area of rectangle
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int l,b;
+//     cin>>l>>b;
+//     cout<<"Area of rectangle is:"<<l*b;
+//     return 0;
+// }
+
+//Q3. Perimeter of rectangle
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int l,b;
+//     cin>>l>>b;
+//     cout<<"Perimeter of rectangle is:"<<2*(l+b);
+//     return 0;
+// }
+
+//Q4.  Area of circle
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int r;
+//     cin>>r;
+//     cout<<"Area of circle is:"<<3.14*r*r;
+//     return 0;
+// }
+
+//Q5. Circumference of circle
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int r;
+//     cin>>r;
+//     cout<<"Circumference of circle is:"<<2*3.14*r;
+//     return 0;
+// }
+
+//Q6. Simple interest
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int p,r,t;
+//     cin>>p>>r>>t;
+//     cout<<"Simple interest is:"<<(p*r*t)/100;
+//     return 0;
+// }
+
