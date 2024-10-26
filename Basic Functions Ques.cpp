2a89eafed56219486d490eff3ece637488b8e2c3
Tanushwi Singh <156989068+Tanushwi@users.noreply.github.com>
2024-10-26 10:07:00 +0530

    Add files via upload

---
+++ Basic Functions Ques.cpp
@@ -0,0 +1,46 @@
+// Q1. Input l and b then print area of rectangle and increement value    of l and b
+// #include<iostream>
+// using namespace std;
+// void area(int x,int y,int &a)
+// {
+//     a=x*y;
+// }
+// void increment(int &x,int &y)
+// {
+//     x=x+2;
+//     y=y+3;
+// }
+// int main()
+// {
+//     int l,b,a;
+//     cout<<"Enter length and breadth:";
+//     cin>>l>>b;
+//     area(l,b,a);
+//     cout<<"Area of rectangle is:"<<a<<endl;
+//     increment(l,b);
+//     cout<<"Updated length and breadth are:"<<l<<" and "<<b;
+//     return 0;
+// }
+
+// Q2. Enter basic salary then calculate bonus and total salary
+// #include<iostream>
+// using namespace std;
+// void bonus(int &bon,int bas)
+// {
+//     bon=0.10*bas;
+// }
+// void total(int bon,int bas,int &tot)
+// {
+//     tot=bas+bon;
+// }
+// int main()
+// {
+//     int basic,bon,tot;
+//     cout<<"Enter basic salary:";
+//     cin>>basic;
+//     bonus(bon,basic);
+//     cout<<"Bonus is:"<<bon<<endl;
+//     total(bon,basic,tot);
+//     cout<<"Total Salary is:"<<tot;
+//     return 0;
+// }
