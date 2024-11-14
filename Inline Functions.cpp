365a091db35c3466533ba7808540ae84455b3492
Tanushwi Singh <156989068+Tanushwi@users.noreply.github.com>
2024-11-14 12:56:58 +0530

    Add files via upload

---
+++ Inline Functions.cpp
@@ -0,0 +1,15 @@
+// Q1. Cube of a number
+//#include<iostream>
+//using namespace std;
+//inline int cube(int n)
+//{
+//	return n*n*n;
+//}
+//int main()
+//{
+//	int num;
+//	cout<<"Enter number:";
+//	cin>>num;
+//	cout<<"Cube is:"<<cube(num);
+//	return 0;
+//}
