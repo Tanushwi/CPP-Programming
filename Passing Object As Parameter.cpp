cdcb8a74028761276641d8f0529c4538a7e0fb1f
Tanushwi Singh <156989068+Tanushwi@users.noreply.github.com>
2024-11-16 20:10:44 +0530

    Add files via upload

---
+++ Passing Object As Parameter.cpp
@@ -0,0 +1,37 @@
+// -> to achieve a=a+b
+
+//#include<iostream>
+//using namespace std;
+//class number
+//{
+//	int x,y;
+//public:
+//	void getdata()
+//	{
+//		cout<<"Enter value of x and y:";
+//		cin>>x>>y;
+//	}
+//	void display()
+//	{
+//		cout<<"\nValue of x:"<<x<<endl;
+//		cout<<"Value of y:"<<y<<endl;
+//	}
+//	void sum(number p)
+//	{
+//		x=x+p.x;
+//		y=y+p.y;
+//	}
+//};
+//int main()
+//{
+//	number a,b;
+//	a.getdata();
+//	b.getdata();
+//	a.sum(b);
+//	a.display();
+//	b.display();
+//	b.sum(a);
+//	a.display();
+//	b.display();
+//	return 0;
+//}
