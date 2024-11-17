1c2f2b96174bd1607e53804151140f274a64d122
Tanushwi Singh <156989068+Tanushwi@users.noreply.github.com>
2024-11-17 10:15:36 +0530

    Add files via upload

---
+++ Destructor Basics.cpp
@@ -0,0 +1,29 @@
+//Basic Destructor Code
+//#include<iostream>
+//using namespace std;
+//int ctr=0;
+//class sample
+//{
+//public:
+//	sample()
+//	{
+//		ctr++;
+//		cout<<"\nConstructor of "<<ctr<<" object executed...";
+//	}
+//	~sample()
+//	{
+//		cout<<"\nDestructor of "<<ctr<<" object executed...";
+//		ctr--;
+//	}
+//};
+//int main()
+//{
+//	sample a,b;
+//	{
+//		sample c;
+//		{
+//			sample d;
+//		}
+//	}
+//	return 0;
+//}
