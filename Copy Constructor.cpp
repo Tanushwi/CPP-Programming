ecd91ffb68bc52a54a602a4304f2a07bef95198f
Tanushwi Singh <156989068+Tanushwi@users.noreply.github.com>
2024-11-17 09:52:07 +0530

    Add files via upload

---
+++ Copy Constructor.cpp
@@ -0,0 +1,44 @@
+//Q. Calculate area of rectangle
+
+//#include<iostream>
+//using namespace std;
+//class rectangle
+//{
+//	int l,b;
+//public:
+//	rectangle()
+//	{
+//		l=0;
+//		b=0;
+//		cout<<"\nDefault Cnstructor executed....";
+//	}
+//	rectangle(rectangle &p)
+//	{
+//		l=p.l;
+//		b=p.b;
+//	}
+//	void setdata(int x,int y)
+//	{
+//		l=x;
+//		b=y;
+//	}
+//	void area()
+//	{
+//		cout<<"\nArea of rectangle is:"<<l*b;
+//	}
+//};
+//int main()
+//{
+//	rectangle r1;
+//	r1.area();
+//	rectangle r4;
+//	r4.setdata(2,3);
+//	r4.area();
+//	rectangle r2=r4;
+//	r4.area();
+//	rectangle r3(r1);
+//	r3.area();
+//	return 0;
+//}
+
+
